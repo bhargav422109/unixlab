04358c4c400a337e2c8c1b7d1be7c9a1053a7f8c
bhargav422109 <167633263+bhargav422109@users.noreply.github.com>
2024-04-20 22:37:06 +0530

    Add files via upload

---
+++ assignment7/caommands.sh
@@ -0,0 +1,49 @@
+# Display content of a file with pagination
+more /home/student/Desktop/422109unix/file.txt
+
+# Number lines of files
+nl /home/student/Desktop/422109unix/file.txt
+# Execute a command with modified scheduling priority
+nice -n 10 ls
+
+# Change user password
+passwd
+
+# Prepare files for printing
+pr /home/student/Desktop/422109unix/file.txt
+
+# Remote login
+rlogin 172.50.0.250
+
+# Copy files between hosts
+rcp /home/student/Desktop/422109unix/file.txt username@hostname:/home/student/Desktop/422109unix/file1.txt
+
+# Remote shell
+rsh 172.50.0.250 ls
+
+# Initiate a conversation with another user
+talk 172.50.0.250
+
+# Connect to another host
+telnet 172.50.0.250
+
+# Set terminal attributes
+tput setaf 2
+
+# Print terminal name
+tty
+
+# Print system information
+uname -a
+
+# Count words, lines, and characters in a file
+wc /home/student/Desktop/422109unix/file.txt
+
+# Display information about currently logged in users
+who
+
+# Write a message to another user
+write student
+
+
+
